2c3c829ed96ff46b0f9b6bc7d84bcb41371fe424
alphanse <alphanse98@gmail.com>
2022-07-06 14:12:59 +0530

    add

---
+++ files/app.js
@@ -0,0 +1,17 @@
+var inc = ()=>{
+    
+    var imgSize = document.getElementById('imgSize');
+    console.log(imgSize);
+     var size = imgSize.clientWidth;
+     (imgSize.style.width = size + 20 + 'px');
+    console.log(x);
+}
+var re = ()=>{
+    
+    var imgSize = document.getElementById('imgSize');
+    console.log(imgSize);
+     var size = imgSize.clientWidth;
+     (imgSize.style.width = size - 20 + 'px');
+    console.log(x);
+}
+
